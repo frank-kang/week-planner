d7ccd525b72bb87ce795a2d91e3fa316a6670aa2
Frank Kang <holycow001@gmail.com>
2025-02-28 20:04:19 +0000

    working on modal form

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 var $days = document.querySelector('.open-modal');
 var $dialog = document.querySelector('dialog');
+var $classCancelModal = document.querySelector('.cancel-modal');
+var $data = ;
 if (!$days)
     throw new Error('$days query failed');
 $days.addEventListener('click', function (event) {
     $dialog.showModal();
+});
+$classCancelModal.addEventListener('click', function () {
+    $dialog.close();
 });
--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
 const $days = document.querySelector('.open-modal');
 const $dialog = document.querySelector('dialog');
+const $classCancelModal = document.querySelector('.cancel-modal');
+const $data =
 
 if (!$days) throw new Error('$days query failed');
 
 $days.addEventListener('click', (event: Event): void => {
   $dialog.showModal();
-  
+});
+
+$classCancelModal.addEventListener('click', () => {
+  $dialog.close();
 });
